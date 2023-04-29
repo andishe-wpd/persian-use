37d9760fdfeed75b3c032f967a543875e0b6c2c2
andishe abbasian <rainbowboxdash@gmail.com>
2023-04-29 17:25:26 +0330

    updated persian number feature

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export { default as useIncludesPersian } from "./components/useIncludesPersian";
 export { default as usePersianDate } from "./components/usePersianDate";
 export { default as usePurePersian } from "./components/usePurePersian";
 export { default as usePersianMobile } from "./components/usePersianMobile";
+export { convert as useSpokenNumber } from "./components/usePersianNumber";
+export { sliceNumber as useSliceNumber } from "./components/usePersianNumber";
+export { convertEnToPe as useConvertEnToPe } from "./components/usePersianNumber";
+export { convertPeToEn as useconvertPeToEn } from "./components/usePersianNumber";
