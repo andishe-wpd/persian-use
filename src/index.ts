c2e69aa2835d06b9fe171b8aa98d468c1f6ea894
andishe abbasian <rainbowboxdash@gmail.com>
2023-04-29 16:45:47 +0330

    formatted

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as usePersianDigits } from "./components/usePersianDigits";
 export { default as useIncludesPersian } from "./components/useIncludesPersian";
 export { default as usePersianDate } from "./components/usePersianDate";
-export {default as usePurePersian} from './components/usePurePersian'
-export {default as usePersianMobile} from './components/usePersianMobile'
+export { default as usePurePersian } from "./components/usePurePersian";
+export { default as usePersianMobile } from "./components/usePersianMobile";
