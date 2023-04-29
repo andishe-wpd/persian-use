51ec38196570f829aff93f4194218cbcaeb4953d
andishe abbasian <rainbowboxdash@gmail.com>
2023-04-29 17:30:45 +0330

    updated duplicate features

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as usePersianDigits } from "./components/usePersianDigits";
 export { default as useIncludesPersian } from "./components/useIncludesPersian";
 export { default as usePersianDate } from "./components/usePersianDate";
 export { default as usePurePersian } from "./components/usePurePersian";
