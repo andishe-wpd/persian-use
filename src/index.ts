6c84dc663239cadc3ab6894cda12bf40d0355c07
andishe abbasian <rainbowboxdash@gmail.com>
2023-04-29 16:37:52 +0330

    added persian mobile name validator

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as usePersianDigits } from "./components/usePersianDigits";
 export { default as useIncludesPersian } from "./components/useIncludesPersian";
 export { default as usePersianDate } from "./components/usePersianDate";
 export {default as usePurePersian} from './components/usePurePersian'
+export {default as usePersianMobile} from './components/usePersianMobile'
