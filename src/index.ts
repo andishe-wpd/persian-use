de17f4d0027dfcc3df35a87aeddbd92ce5df8ada
andishe abbasian <rainbowboxdash@gmail.com>
2023-04-30 16:19:33 +0330

    updated index fearures

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { default as useIncludesPersian } from "./components/useIncludesPersian";
 export { default as usePersianDate } from "./components/usePersianDate";
-export { default as usePurePersian } from "./components/usePurePersian";
 export { default as usePersianMobile } from "./components/usePersianMobile";
 export { convert as useSpokenNumber } from "./components/usePersianNumber";
 export { sliceNumber as useSliceNumber } from "./components/usePersianNumber";
@@ -8,3 +7,9 @@ export { convertEnToPe as useConvertEnToPe } from "./components/usePersianNumber
 export { convertPeToEn as useconvertPeToEn } from "./components/usePersianNumber";
 export { default as usePersianSlang } from "./components/usePersianSlang";
 export { default as usePersianCurrency } from "./components/usePersianCurrency";
+export { default as usePurePersian } from "./components/UsePurePersian";
+export { default as usePersianSpeaker } from "./components/usePersianSpeaker";
+export {
+  useFinglishToPersian,
+  usePersianToFinglish,
+} from "./components/useFinglishEnglishConvertor";
