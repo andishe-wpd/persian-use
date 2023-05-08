fd119ff5ef2672467d959ffb3857124f8f22b5a0
andishe abbasian <rainbowboxdash@gmail.com>
2023-05-08 15:44:23 +0330

    updated persian test date

--- src/tests/usePersianDate.test.ts
+++ src/tests/usePersianDate.test.ts
@@ -5,7 +5,7 @@ describe("usePersianDate", () => {
     // Define a test date for Tehran timezone (UTC+4:30)
     const testDateTehran = new Date("2023-05-01T15:30:00+04:30");
     // Define the expected Persian date string for Tehran timezone
-    const expectedStringTehran = "۱۴۰۲ اردیبهشت ۱۱, دوشنبه، ساعت ۱۴:۳۰";
+    const expectedStringTehran = "۱۴۰۲ اردیبهشت ۱۱, دوشنبه، ساعت ۱۱:۰۰";
     // Call the `usePersianDate` function with the test date in Tehran timezone
     const persianDateStringTehran = usePersianDate(testDateTehran);
     // Test that the result matches the expected Persian date string for Tehran timezone
